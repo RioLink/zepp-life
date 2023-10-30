85ebb0fb898255ebc5378f9edb84bec46061699f
Alex <oleksandr.kudelya96@gmail.com>
2023-10-31 00:11:39 +0200

    DB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
+
+const db = require('./database');
 
 app.get('/', (req, res) => {
   res.send('ZEPP-LIFE');
