f8083aff290ef90d7068c113298a00081ef9ee01
Alex <oleksandr.kudelya96@gmail.com>
2023-10-31 00:17:13 +0200

    DB1

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
   res.send('ZEPP-LIFE');
 });
 
+app.use(express.json());
+app.use('/api', require('./routes/userRoutes'));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
